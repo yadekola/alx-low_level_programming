a5e42a098b4a468ee8cc94ca3b2395a73bd7f65d
yadekola <yakubadekolaojo059@gmail.com>
2022-10-15 08:18:59 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point of every C program
+ * Return: (0)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multiligual puzzle");
+	return (0);
+}
