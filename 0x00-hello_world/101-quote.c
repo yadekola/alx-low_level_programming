8104ef5b75a8617bd49a954f15c786cd0e91a7c0
yadekola <yakubadekolaojo059@gmail.com>
2022-10-15 10:23:37 -0700

    unix os

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,11 @@
 #include <unisted.h>
 
 /**
- * main - prints the text "and that piece of art is useful"
- * -Dora Korpar, 2015-10-19",
- *  folloed by a nw line, to the standard error.
- *  Return: Always 0 (Success)
- *  */
+ * main - Entry point
+ *  Return: Always 1
+ */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
