b9c39e36d5432dbefb62b09d279fb1f69ab00492
yadekola <yakubadekolaojo059@gmail.com>
2022-10-19 16:33:13 -0700

    origin

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - This function adds two intergers
+* @one: 1st number
+* @two: 2nd number
+* Return: one + two
+*/
+
+int add(int one, int two)
+{
+	return (one + two);
+}
+
