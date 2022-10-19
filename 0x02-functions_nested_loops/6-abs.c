e6132e04156c95f5c76ed899cc4d0bd90176e788
yadekola <yakubadekolaojo059@gmail.com>
2022-10-19 16:16:17 -0700

    estimat

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _abs - This function calculates absolute numbers from integers
+* @ab: Represents entered integers
+* Return: This will return the calculated absolute value
+*/
+
+int _abs(int ab)
+
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+}
