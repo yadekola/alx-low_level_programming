21023859c1be86a37980b87cbe586698d1a35374
yadekola <yakubadekolaojo059@gmail.com>
2022-10-20 16:32:51 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - returns the result
+* @a: an int from the main function
+* @b: an int from the main function
+* Chinweike Okwuduche
+* Return: result
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
