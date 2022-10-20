fb6ef2077bfc463b004a1732f8cf8c76ddf57916
yadekola <yakubadekolaojo059@gmail.com>
2022-10-20 16:38:56 -0700

    void print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers - given sets of numbers
+* Authour: Chinweike Okwuduche
+* Return: void - empty
+*/
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
