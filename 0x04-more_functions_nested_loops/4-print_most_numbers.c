6244e7b99d305720dff0315f3881c1485a081c27
yadekola <yakubadekolaojo059@gmail.com>
2022-10-20 16:47:17 -0700

    signs23

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints 0-9 with a new line
+* Return: nothing - void
+*/
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	_putchar('\n');
+}
