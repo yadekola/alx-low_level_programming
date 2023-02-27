bac42807e970efbfb700574182a2ecec1e7c70a3
yadekola <yakubadekolaojo059@gmail.com>
2023-02-27 15:38:50 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* reset_to_98 - reset_to_98 i.e manipulates pointer's value
+* @n: Acts as pointer resetter
+* Return: void
+*/
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
