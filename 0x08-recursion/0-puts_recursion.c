3d087ef1570d876e6f67f23e2d0e976786401a32
Adekola <111663128+yadekola@users.noreply.github.com>
2023-03-08 03:26:01 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse
- * @s: Represents string
- * Return: Returns 1 if all goes well
+ * _puts_recursion - Print a string
+ * @s: string
  */
-void _print_rev_recursion(char *s)
+
+void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
+		_putchar('\n');
+	}
+	else
+	{
 		_putchar(*s);
+		_puts_recursion(++s);
 	}
 }
